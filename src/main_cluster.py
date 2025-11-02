9cbf18ef68d8bbc3f2dd7dcc8e669872bd9e748a
Chris Wendler <chris.wendler.mobile@gmail.com>
2025-11-02 16:07:38 -0500

    argparse

--- src/main_cluster.py
+++ src/main_cluster.py
@@ -2,11 +2,29 @@ from .datasets.pong1m import get_loader
 from .models.dit import get_model
 from .trainers.rectified_flow import train
 import wandb
+import argparse
 
 import torch as t
 t.set_float32_matmul_precision("high")
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()    # 0.002, 3e-5, (0.9, 0.95), 1e-5, 26000 works ok
+    parser.add_argument("--lr1", type=float, default=0.002)
+    parser.add_argument("--lr2", type=float, default=3e-5)
+    parser.add_argument("--betas", type=tuple, default=(0.9, 0.95))
+    parser.add_argument("--weight_decay", type=float, default=1e-5)
+    parser.add_argument("--max_steps", type=int, default=26000)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--duration", type=int, default=1)
+    parser.add_argument("--fps", type=int, default=7)
+    parser.add_argument("--debug", type=bool, default=False)
+    parser.add_argument("--n_window", type=int, default=7)
+    parser.add_argument("--patch_size", type=int, default=2)
+    parser.add_argument("--n_heads", type=int, default=4)
+    parser.add_argument("--d_model", type=int, default=64)
+    parser.add_argument("--n_blocks", type=int, default=12)
+    parser.add_argument("--T", type=int, default=1000)
+    args = parser.parse_args()
     wandb.init(project="toy-wm")
     # Detect MPS (Apple Silicon) or CUDA if available
     if t.backends.mps.is_available():
@@ -19,10 +37,10 @@ if __name__ == "__main__":
         device = t.device("cpu")
         print("Using device: CPU")
 
-    loader, _, _ = get_loader(batch_size=32, duration=1, fps=7)
+    loader, _, _ = get_loader(batch_size=args.batch_size, duration=args.duration, fps=args.fps, debug=args.debug) # 7 was the max that does not go oom
     frames, actions = next(iter(loader))
     height, width = frames.shape[-2:]
-    model = get_model(height, width, n_window=7, patch_size=2, d_model=64, n_blocks=12)
+    model = get_model(height, width, n_window=args.n_window, patch_size=args.patch_size, n_heads=args.n_heads,d_model=args.d_model, n_blocks=args.n_blocks, T=args.T)
     model = model.to(device)  # Move model to device
 
     # Apply torch compile for acceleration (PyTorch 2.0+)
@@ -35,8 +53,7 @@ if __name__ == "__main__":
     #model = model.to(t.bfloat16)
     # Pass device to train if needed, or make sure trainer and dataloader use device
     # wandb.watch(model, log="all", log_freq=100)  # log_freq reduces logging overhead, log="all" avoids gradient tracking issues
-    model = train(model, loader, lr1=0.01, lr2=1.5e-4, betas=(0.9, 0.95), weight_decay=1e-5, max_steps=26000)
-    # 0.002, 3e-5, (0.9, 0.95), 1e-5, 26000 works ok
+    model = train(model, loader, lr1=args.lr1, lr2=args.lr2, betas=args.betas, weight_decay=args.weight_decay, max_steps=args.max_steps)
 
     import os
     from datetime import datetime
