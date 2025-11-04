d2e1bcdfd86171ddf3f3c10a015f1c2c65573f76
Chris Wendler <chris.wendler.mobile@gmail.com>
2025-11-04 14:29:15 -0500

    configs

--- src/nn/geglu.py
+++ src/nn/geglu.py
@@ -6,10 +6,13 @@ class GEGLU(nn.Module):
         self.d_in = d_in
         self.d_mid = d_mid
         self.d_out = d_out
-        self.up_proj = nn.Linear(d_in, d_mid)
-        self.up_gate = nn.Linear(d_in, d_mid)
-        self.down = nn.Linear(d_mid, d_out)
-        self.nonlin = nn.GELU()
+        self.up_proj = nn.Linear(d_in, d_mid, bias=True)
+        self.up_proj.bias.data.zero_()
+        self.up_gate = nn.Linear(d_in, d_mid, bias=True)
+        self.up_gate.bias.data.zero_()
+        self.down = nn.Linear(d_mid, d_out, bias=True)
+        self.down.bias.data.zero_()
+        self.nonlin = nn.SiLU()
     
     def forward(self, x):
         x = self.up_proj(x) * self.nonlin(self.up_gate(x))
