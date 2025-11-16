f352841d022a07fddece1e50e94abd49786c4b5a
Chris Wendler <chris.wendler.mobile@gmail.com>
2025-11-15 22:36:49 -0500

    flashattn working

--- src/nn/attn2.py
+++ src/nn/attn2.py
@@ -10,7 +10,7 @@ except ImportError:
     _has_flashattn = False
 
 class Attention(nn.Module):
-    def __init__(self, d_model, n_heads, causal=True, debug=False, use_flash=False):
+    def __init__(self, d_model, n_heads, causal=True, debug=False, use_flash=True, rope=None):
         super().__init__()
         self.d_model = d_model
         self.n_heads = n_heads
@@ -21,6 +21,9 @@ class Attention(nn.Module):
 
         self.QKV = nn.Linear(self.d_model, 3 * self.d_model)
         self.O = nn.Linear(self.d_model, self.d_model)
+        self.lnq = nn.LayerNorm(self.d_head)
+        self.lnk = nn.LayerNorm(self.d_head)
+        self.rope = rope
         if use_flash and not _has_flashattn:
             raise ImportError("flash-attn is not installed.")
         self.use_flash = use_flash
@@ -30,24 +33,29 @@ class Attention(nn.Module):
         qkv = self.QKV(x)
         q, k, v = qkv.chunk(3, dim=-1)
         b, s, d = q.shape
-        q = q.reshape(b, s, self.n_heads, self.d_head).permute(0, 2, 1, 3)
-        k = k.reshape(b, s, self.n_heads, self.d_head).permute(0, 2, 1, 3)
-        v = v.reshape(b, s, self.n_heads, self.d_head).permute(0, 2, 1, 3)
-        # q, k, v: (batch, n_heads, seq, d_head)
+        q = q.reshape(b, s, self.n_heads, self.d_head)
+        k = k.reshape(b, s, self.n_heads, self.d_head)
+        v = v.reshape(b, s, self.n_heads, self.d_head)
+        q = self.lnq(q)
+        k = self.lnk(k)
+        if self.rope is not None:
+            q = self.rope(q)
+            k = self.rope(k)
         if self.use_flash:
             # flash_attn_func expects shape (batch, seqlen, nheads, d_head) and fp16/bf16 on CUDA
-            q_flash = q.permute(0, 2, 1, 3).contiguous()
-            k_flash = k.permute(0, 2, 1, 3).contiguous()
-            v_flash = v.permute(0, 2, 1, 3).contiguous()
+            q_flash = q.contiguous()
+            k_flash = k.contiguous()
+            v_flash = v.contiguous()
             # shape: (batch, seq, n_heads, d_head)
             # flash_attn_func(q, k, v, dropout_p, softmax_scale, causal)
             softmax_scale = self.d_head ** (-0.5)
-            y = flash_attn_func(q_flash, k_flash, v_flash, 0.0, softmax_scale, self.causal)
+            z = flash_attn_func(q_flash, k_flash, v_flash, 0.0, softmax_scale, self.causal)
             # output: (batch, seq, n_heads, d_head) --> we don't need to permute to that order
-            z = y.reshape(b, s, d)
-            z = self.O(z)
-            return z
         else:
+            q = q.permute(0, 2, 1, 3)
+            k = k.permute(0, 2, 1, 3)
+            v = v.permute(0, 2, 1, 3)
+            # q, k, v: (batch, n_heads, seq, d_head)
             attn = q @ k.permute(0, 1, 3, 2) * self.d_head ** (-0.5)  # batch x nh x seqq x seqk
             if self.causal:
                 attn = t.where(self.mask(s), attn, float("-inf"))
@@ -57,9 +65,10 @@ class Attention(nn.Module):
                 plt.show()
             z = probas @ v
             # z ... batch x nh x seq x dh
-            z = z.permute(0, 2, 1, 3).reshape(b, s, d)
-            z = self.O(z)
-            return z
+            z = z.permute(0, 2, 1, 3)
+            # z ... batch x seq x nh x dh
+        z = self.O(z.reshape(b, s, d))
+        return z
 
     def mask(self, s: int):
         return t.tril(t.ones((s, s), dtype=bool, device=self.device))
@@ -78,16 +87,16 @@ if __name__ == "__main__":
     # Flash attention requires CUDA
     device = 'cuda' if t.cuda.is_available() and _has_flashattn else 'cpu'
     
-    attn = Attention(32, 2, use_flash=False).to(device)
-    attn_flash = Attention(32, 2, use_flash=True).to(device) if _has_flashattn and t.cuda.is_available() else None
+    attn = Attention(384, 6, use_flash=False).to(device)
+    attn_flash = Attention(384, 6, use_flash=True).to(device) if _has_flashattn and t.cuda.is_available() else None
     
     # Flash attention requires fp16 or bf16
     if _has_flashattn and t.cuda.is_available():
         attn = attn.to(t.bfloat16)
         attn_flash = attn_flash.to(t.bfloat16)
-        x = t.rand(8, 10, 32, device=device, dtype=t.bfloat16)
+        x = t.rand(32, 65*30, 384, device=device, dtype=t.bfloat16)
     else:
-        x = t.rand(8, 10, 32, device=device)
+        x = t.rand(32, 65*30, 384, device=device)
     
     y_ref = attn(x)
     
