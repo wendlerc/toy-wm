b05c89eba9b27da50bfe33614b644166fcf33eb8
Chris Wendler <chris.wendler.mobile@gmail.com>
2025-11-23 11:13:03 -0500

    starting with a dummy action does not really make sense in the sliding window setting

--- src/datasets/pong1m.py
+++ src/datasets/pong1m.py
@@ -13,12 +13,13 @@ def get_loader(batch_size=64, fps=30, duration=5, shuffle=True, debug=False, dro
     frames = t.from_numpy(np.load("./datasets/pong1M/frames.npy"))
     actions = t.from_numpy(np.load("./datasets/pong1M/actions.npy"))
     height, width, channels = frames.shape[-3:]
-    n = frames.shape[0]//(fps*duration) 
-    frames = frames[:n*fps*duration]
-    frames = frames.reshape(n, fps*duration, height, width, channels)
+    frames_per_example = fps*duration + 1
+    n = frames.shape[0]//frames_per_example
+    frames = frames[:n*frames_per_example]
+    frames = frames.reshape(n, frames_per_example, height, width, channels)
     frames = frames.permute(0, 1, 4, 2, 3)
-    actions = actions[:n*fps*duration]
-    actions = actions.reshape(-1, fps*duration)
+    actions = actions[:n*frames_per_example]
+    actions = actions.reshape(-1, frames_per_example)
     b, dur, c, h, w = frames.shape
     # preprocess
     z = rearrange(frames, "b dur c h w -> (b dur h w) c")
@@ -29,9 +30,12 @@ def get_loader(batch_size=64, fps=30, duration=5, shuffle=True, debug=False, dro
     frames = z
     pred2frame = fixed2frame
 
+    # we add 1 so 0 becomes our dummy action for CFG
     actions += 1
-    actions[:, 1:] = actions[:, :-1] 
-    actions[:, :1] = 0
+    # each frame gets as a condition the action of its previous frame
+    frames = frames[:, 1:]
+    actions = actions[:, :-1] 
+
     # 0... dummy
     # 1... do nothing
     # 2... up
